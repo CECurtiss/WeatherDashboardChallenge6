f701a9579a86a778a5bea341c8b1b3d627da4b13
CECurtiss <curtissclinton@gmail.com>
2022-10-21 23:20:51 -0500

    stuck trying to get info from HTML page. abandoned jquery

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
-var searchBtn = $('#searchbtn');
-var userInput = $('#userinput').val;
-var fiveDayForecast = $('.fiveday');
-var searchHist = $('.searchhistory');
+var searchBtn = document.querySelector('#searchbtn');
+var userInput = document.querySelector('#userinput').value;
+var fiveDayForecast = document.querySelector('.fiveday');
+var searchHist = document.querySelector('.searchhistory');
 
 var submitUserEntry = function (userInput) {
+console.log(userInput);
+    var geocodeTranslator = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + ",840&limit=5&appid=1878dc2f6221aa2b08efb2c0a1e2da79";
 
-    var geocodeTranslator = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=1878dc2f6221aa2b08efb2c0a1e2da79";
     fetch(geocodeTranslator)
         .then(function (response) {
             if (response.ok) {
@@ -17,12 +18,18 @@ var submitUserEntry = function (userInput) {
         })
         .then(function (coords) {
         console.log(coords);
-        storeCoords(userInput, coords);
+        storeCoords(coords);
         }) 
         .catch(function() {
             alert("Unable to connect to Server");
         });
 }
+
+function storeCoords() {
+    // storing coords in variable to pass to request.url
+    
+}
+
 function getApi () {
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=1878dc2f6221aa2b08efb2c0a1e2da79"
 
@@ -33,4 +40,4 @@ function getApi () {
 })
 }
 
-$(searchBtn).click(sumbitUserEntry);
+searchBtn.addEventListener("click", submitUserEntry);
